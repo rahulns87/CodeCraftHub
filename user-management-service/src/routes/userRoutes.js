0d9403fb9ae5550cf3623537e467628ff7117e12
rahulns87 <rahul6030@gmail.com>
2025-11-08 04:03:56 -0500

    Docker updates

--- user-management-service/src/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-// src/routes/userRoutes.js
-const express = require('express');
-const { registerUser, loginUser } = require('../controllers/userController');
-const router = express.Router();
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-module.exports = router;
--- user-management-service/src/services/userService.js
+++
@@ -1,6 +0,0 @@
-// src/services/userService.js
-const User = require('../models/userModel');
-// Function to find user by ID
-exports.findUserById = async (userId) => {
-    return await User.findById(userId);
-};
